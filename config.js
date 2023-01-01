b47125987efc2ee134fd05c2415d400188530040
Agubgstore <117073589+Agubgstore@users.noreply.github.com>
2023-01-01 17:08:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoRecord = true //auto recording (true to on, false to off)
 
 //documents variants
@@ -14,24 +14,24 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6285730659421'] //ur owner number
+global.ownername = "🦄AGUNG" //ur owner name
+global.ytname = "YT: Agung GT" //ur yt chanel name
+global.socialm = "GitHub: Agubgstore" //ur github or insta name
+global.location = "Indonesia" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
+global.owner = ['6285730659421'] //ur number
+global.ownernomer = "6285730659421" //ur number
+global.ownertag = '6285730659421' //ur tag number
+global.premium = ['6285730659421'] //ur premium number
+global.botname = 'Trinex Bot' //ur bot name
+global.ownername = "🦄AGUNG"
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
+global.packname = "Sticker By AGUNG" //ur sticker watermark packname
 global.author = "Cheems Bot" //ur sticker watermark author
 global.wm = "Xeon Bot Inc." //ur watermark
 
